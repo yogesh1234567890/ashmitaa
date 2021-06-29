1e3779e56d5ccf3376837db43bf71243c232983c
yogesh1234567890 <yogeshbhattarai073gmail.com>
2021-06-29 14:47:02 +0545

    onesessionperuser added

--- blog/settings.py
+++ blog/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 
-    'home.middlewares.OneSessionPerUser',
+    # 'home.middlewares.OneSessionPerUser',
 
 ]
 
